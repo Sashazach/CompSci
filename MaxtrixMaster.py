b238752c064d26c1a101051515ba611d69d31296
Sashazach <zbostock26@gcds.net>
2024-11-11 21:17:29 -0500

    test

---
+++ MaxtrixMaster.py
@@ -0,0 +1,31 @@
+class MatrixMaster:
+    def __init__(self, rows, cols):
+        self.cols = cols
+        self.rows = rows
+        self.data = [[0 for _ in range(cols)] for _ in range(rows)]
+
+    def plus(self, matrix : 'MatrixMaster') -> 'MatrixMaster':
+        if matrix.rows != self.rows or matrix.cols != self.cols: return
+        newMatrix = MatrixMaster(self.rows, self.cols)
+        for i in range(matrix.rows):
+            for j in range(matrix.cols):
+                newMatrix.data[i][j] = matrix.data[i][j] + self.data[i][j]
+        return newMatrix
+            
+    def multiply(self, matrix : 'MatrixMaster') -> 'MatrixMaster':
+        newMatrix = MatrixMaster(self.rows, matrix.cols)
+        for i in range(self.rows):
+            for j in range(matrix.cols):
+                for k in range(self.cols):
+                    newMatrix.data[i][j] = self.data[i][k]*matrix.data[k][j]
+        return newMatrix
+    
+    def print_matrix(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                print(self.data[i][j], end="")
+            print()
+        
+
+        
+        
---
+++ matrixTester.py
@@ -0,0 +1,15 @@
+import MaxtrixMaster
+
+ralph = MaxtrixMaster.MatrixMaster(3, 4)
+ralph.data = [
+    [3, 4, 5, 3],
+    [5, 2, 1, 0],
+    [4, 4, 4, 5]
+]
+
+trixie = MaxtrixMaster.MatrixMaster(3, 4)
+trixie.data = [
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 1, 1, 1],
+]
